3219ca642547e5c30d296803303ba3fcd9189f38
Matthew Gonzales <mattgonzales3@yahoo.com>
2025-03-24 14:33:52 -0500

    debugging playerMove

---
+++ HelloWold.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
+
+int main() 
+{
+
+    char spaces[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    while (running)
+    {
+        playerMove(spaces, player);
+        drawBoard(spaces);
+    }
+    
+
+    return 0;
+}
+
+void drawBoard(char *spaces){
+    std::cout << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << " " << spaces[0] << "   |  " << spaces[1] << "  |  " << spaces[2] << "  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << " " << spaces[3] << "   |  " << spaces[4] << "  |  " << spaces[5] << "  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << " " << spaces[6] << "   |  " << spaces[7] << "  |  " << spaces[8] << "  " << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << '\n';
+}
+void playerMove(char *spaces, char player){
+    int number;
+    do
+    {
+        std::cout << "enter a spot to place ur marker (1-9): ";
+        std::cin >> number;
+        number--;
+        if (spaces[number] == ' ')
+        {
+            spaces[number] = player;
+            break;
+        }
+        
+    } while (!(number > 0) || !(number < 8));
+    
+}
+void computerMove(char *spaces, char computer){
+
+}
+bool checkWinner(char *spaces, char player, char computer){
+
+    return 0;
+}
+bool checkTie(char *spaces){
+
+    return 0;
+}
